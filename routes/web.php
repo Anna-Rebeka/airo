0a59d4efc860796db56df6436bf88080ba655245
Anna-Rebeka <rebeka.horecka@gmail.com>
2021-11-29 22:38:54 +0100

    flight searching

--- routes/web.php
+++ routes/web.php
@@ -30,4 +30,4 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/from/{letters}', [App\Http\Controllers\CityController::class, 'findByName']);
 Route::get('/search', [App\Http\Controllers\FlightController::class, 'index']);
-Route::get('/flights/{from}/{to}/{when}/{till}/{price}', [App\Http\Controllers\FlightController::class, 'searchFlights']);
+Route::get('/flights/{from}/{to}/{when}/{price}', [App\Http\Controllers\FlightController::class, 'searchFlights']);
