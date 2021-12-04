e30eba62cd5c62e8d2a4251752c757b0f9005643
bl96 <47901930+bl96@users.noreply.github.com>
2021-12-04 23:57:34 +0100

    Create GalleryImpl.tsx

---
+++ resources/js/components/gallery/GalleryImpl.tsx
@@ -0,0 +1,15 @@
+import React, {FunctionComponent} from "react";
+
+interface Props {
+
+}
+
+
+export const GalleryImpl: FunctionComponent<Props> = ({
+                                                          children
+                                                      }) => {
+    return (
+        <>
+        </>
+    )
+}
