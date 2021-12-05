28276ecfbf15fa03968898caeb53a8f9e915cdc9
bl96 <47901930+bl96@users.noreply.github.com>
2021-12-05 20:21:05 +0100

    Update ModularForm.tsx

--- resources/js/components/input/modular-form/ModularForm.tsx
+++ resources/js/components/input/modular-form/ModularForm.tsx
@@ -11,20 +11,20 @@ let FormWrapper = styled.div<{ shouldBeDisplayed: boolean }>`
     overflow: auto;
     width: 100vw;
     height: 100vh;
-    left:0;
-    right:0;
-    top:0;
-    /*left: 25vw;*/
-    background-color: rgba(125, 125, 125, 0.5);
+    left: 0;
+    right: 0;
+    top: 0;
+    background: rgba(26, 26, 20, 0.8);
     text-align: center;
     align-items: center;
     justify-content: center;
+    z-index: 9999;
 `;
 
 let Reg = styled.form`
     display: block;
     text-align: center;
-    border: 2px crimson solid;
+    border: 1px white solid;
     position: relative;
     width: 50%;
     height: 50%;
@@ -71,31 +71,83 @@ let Close = styled.button`
     float: right;
     cursor: pointer;
     background-color: rgba(125, 125, 125, 0);
-    border: 1px rgba(0, 0, 0, 0) solid;
+    border: 1px black solid;
     margin: 5px;
+    font-size: 1.5em;
 
     :hover, :focus {
-        border-color: rgba(125, 125, 125, 1);
+        border-color: white;
     }
+`;
+
+let InputWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+
+let Title = styled.h1`
+    color: white;
+    text-align: center;
+`
+
+let RegistrationButton = styled.button`
+    text-decoration: none;
+    border: 0.1em solid white;
+    color: white;
+    font-size: 1.2em;
+    cursor: pointer;
+    background-color: transparent;
+    padding: 0.5rem 1rem;
+    font-weight: bold;
+    text-shadow: 0 5px 8px black;
+
+    :hover {
+        color: #FF7F2A;
+        border: 0.1em solid #FF7F2A;
+    }
+
+    @media (min-width: 772px) {
+        font-size: 1.4em;
+    }
+
+    @media (min-width: 992px) {
+        font-size: 1.6em;
+    }
+
+    @media (min-width: 1280px) {
+        font-size: 1.8em;
+    }
+
+    @media (min-width: 1920px) {
+        font-size: 2em;
+    }
+    transition: background-color 0.2s, color 0.3s;
 `;
 
 export const ModularForm: FunctionComponent<Props> = ({}) => {
     let [display, setDisplay] = useState(false);
+    let [canClose, setCanClose] = useState(true);
     return (
         <>
-            <FormWrapper shouldBeDisplayed={display} onSubmit={(e) => e.preventDefault()}>
-                <Reg id='reg'>
+            <FormWrapper shouldBeDisplayed={display} onSubmit={(e) => e.preventDefault()} onClick={() => {
+                if (canClose) {
+                    setDisplay(false);
+                }
+
+            }}>
+                <Reg id='reg' onMouseEnter={() => setCanClose(false)} onMouseLeave={() => setCanClose(true)}>
                     <Close id="hideBtn" onClick={() => setDisplay(false)}>X</Close>
-                    <h1> Registrácia </h1>
-                    <br/>
-                    <Username type="text" id="username" name="username" placeholder="User name"></Username>
-                    <br/>
-                    <Password type="password" id="pw" name="pw" placeholder="Password"></Password>
-                    <br/>
-                    <Submit type="submit" id="submit" name="submit" value="submit"></Submit>
+                    <Title> Registration </Title>
+                    <InputWrapper>
+                        <Username type="text" id="username" name="username" placeholder="User name"/>
+                        <Password type="password" id="pw" name="pw" placeholder="Password"/>
+                        <Submit type="submit" id="submit" name="submit" value="submit"/>
+                    </InputWrapper>
                 </Reg>
             </FormWrapper>
-            <button id="showBtn" onClick={() => setDisplay(true)}> Registrácia</button>
+            <RegistrationButton id="showBtn" onClick={() => setDisplay(true)}> Registration</RegistrationButton>
         </>
     );
 }
