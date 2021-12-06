e01a46b7cd76017350d06c3548c7418d30caeced
Anna-Rebeka <rebeka.horecka@gmail.com>
2021-12-06 20:50:51 +0100

    tickets

--- resources/views/flights/index.blade.php
+++ resources/views/flights/index.blade.php
@@ -1,3 +1,28 @@
-<x-master>
-    <div id="search_flights" data-user="{{ json_encode($user) }}">
-</x-master>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+   <head>
+         <meta charset="utf-8">
+         <meta name="viewport" content="width=device-width, initial-scale=1">
+
+         <title>Airo+</title>
+
+         <!-- Fonts -->
+         <link href="https://fonts.gstatic.com" rel="preconnect">
+         <link as="style" href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+                  onload="this.onload = null; this.rel='stylesheet'" rel="preload">
+
+         <!-- Styles -->
+         <style>
+             html, body {
+                 font-family: 'Roboto', sans-serif;
+                 margin: 0;
+                 height: 100vh;
+             }
+         </style>
+     </head>
+     <body>
+         <div id="myflights" data-user="{{ json_encode(auth()->user()) }}" data-user="{{ json_encode($tickets) }}">
+         </div>
+         <script src="<?php echo e(mix('js/app.js')); ?>"></script>
+     </body>
+</html>
