9bfa913dc6acfd8f8b7ddc2d24fe274c4948a0bb
marekdurana <durana13@uniba.sk>
2021-12-07 15:01:12 +0100

    update login/register

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\CustomAuthController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -25,7 +25,12 @@ Route::group(['middleware' => ['auth']], function() {
  });
 
 
-Auth::routes();
+ Route::get('dashboard', [CustomAuthController::class, 'dashboard']); 
+ Route::get('login', [CustomAuthController::class, 'index'])->name('login');
+ //Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom'); 
+ Route::get('registration', [CustomAuthController::class, 'registration'])->name('register-user');
+ //Route::post('custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom'); 
+ Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/from/{letters}', [App\Http\Controllers\CityController::class, 'findByName']);
