327666b990a3e7cdf0299790d299ac4a518e8cc5
bl96 <47901930+bl96@users.noreply.github.com>
2021-12-12 15:52:31 +0100

    added contacts

--- resources/js/components/sections/ContactsImpl.tsx
+++ resources/js/components/sections/ContactsImpl.tsx
@@ -1,6 +1,9 @@
 import React, {FunctionComponent, useEffect, useState} from "react";
 import BasicImpl from "./BasicImpl";
 import styled from "@emotion/styled";
+import {GoogleMaps} from "../map/GoogleMap";
+import {Heading1} from "../heading/Heading1";
+import {Heading2} from "../heading/Heading2";
 
 interface Props {
     dataset: any;
@@ -12,10 +15,21 @@ let Wrapper = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: center;
     background-color: #000000;
     background-image: linear-gradient(0deg, #000000 0%, #404040 100%);
 `
+
+let ContactComponent = styled.address`
+    width: 100%;
+    text-align: center;
+    color: white;
+    font-size: 1.4em;
+    margin-bottom: 1em;
+
+    @media (min-width: 476px) {
+        width: 25%;
+    }
+`;
 
 export const ContactsImpl: FunctionComponent<Props> = ({
                                                            dataset,
@@ -30,7 +44,14 @@ export const ContactsImpl: FunctionComponent<Props> = ({
     return (
         <BasicImpl user={user} setUser={setUser} id={"contacts"}>
             <Wrapper>
-
+                <Heading1>Contacts</Heading1>
+                <Heading2>You can find us on</Heading2>
+                <ContactComponent>
+                    Airline search engine plus<br/>
+                    Bratislavská 47<br/>
+                    Bratislava<br/>
+                </ContactComponent>
+                <GoogleMaps title={"Map"}/>
             </Wrapper>
         </BasicImpl>
     )
