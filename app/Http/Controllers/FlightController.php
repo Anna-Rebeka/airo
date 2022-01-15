ac0190bbe668a0cc521c73daef962e9accc3da25
Anna-Rebeka <rebeka.horecka@gmail.com>
2022-01-15 12:58:22 +0100

    create flight

--- app/Http/Controllers/FlightController.php
+++ app/Http/Controllers/FlightController.php
@@ -22,6 +22,48 @@ class FlightController extends Controller
         ]);
     }
 
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store($city1, $city2, $leaves)
+    {   
+        $long1 = deg2rad($city1->lng);
+        $long2 = deg2rad($city2->lng);
+        $lat1 = deg2rad($city1->lat);
+        $lat2 = deg2rad($city2->lat);
+
+        $dlong = $long2 - $long1;
+        $dlati = $lat2 - $lat1;
+
+        $val = pow(sin($dlati/2),2)+cos($lat1)*cos($lat2)*pow(sin($dlong/2),2);
+
+        $res = 2 * asin(sqrt($val));
+        $radius = 3958.756;
+
+        $distance = $res * $radius;
+        $company = Company::all()->random(1)->first();
+        $price =  $distance * (0.05 + 0.001 * $company->class +  0.001 * rand(1,10));
+        
+        $time = new DateTime($leaves->format('Y-m-d H:i'));
+        $arrives = $time->add(new DateInterval('PT' . $minutes . 'M'));
+
+        $flight = Flight::create([
+            'name' => $this->faker->bothify('?###??##'),
+            'departure_id' => $city1->id,
+            'arrival_id' => $city2->id,
+            'company_id' => $company->id,
+            'leaves' => $leaves,
+            'arrives' => $arrives,
+            'price' => $price,
+            'distance' => $distance
+        ]);
+        
+        $flight->departure;
+        $flight->arrival;
+
+        return $flight;
+    }
+
 
     public function getFlights($from, $to, $when, $price)
     {
