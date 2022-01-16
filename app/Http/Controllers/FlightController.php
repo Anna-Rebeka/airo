1df2b7bea97408af23dbaaa897e8a12671c95e96
Anna-Rebeka <rebeka.horecka@gmail.com>
2022-01-16 18:05:42 +0100

    preferences and price

--- app/Http/Controllers/FlightController.php
+++ app/Http/Controllers/FlightController.php
@@ -37,7 +37,7 @@ class FlightController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store($city1, $city2, $leaves)
+    public function store($city1, $city2, $leaves, $price)
     {   
         $long1 = deg2rad($city1->lng);
         $long2 = deg2rad($city2->lng);
@@ -54,12 +54,12 @@ class FlightController extends Controller
 
         $distance = $res * $radius;
         $company = Company::all()->random(1)->first();
-        $price =  $distance * (0.05 + 0.001 * $company->class +  0.001 * rand(1,10));
         
         $time = new DateTime($leaves);
         $minutes = round(($distance / 830) * 60);
         $arrives = $time->add(new DateInterval('PT' . $minutes . 'M'));
         $arrives = $arrives->format('Y-m-d H:i');
+        
         $flight = Flight::create([
             'name' => $this->faker(),
             'departure_id' => $city1->id,
@@ -84,7 +84,6 @@ class FlightController extends Controller
         $city1 = City::where('name', $from)->get()->first();
         $city2 = City::where('name', $to)->get()->first();
 
-        //treba vyrobit
         $theDate = new DateTime($when);
         $morning = $theDate->format('Y-m-d H:i:s');
 
@@ -104,10 +103,11 @@ class FlightController extends Controller
         return $flights;
     }
 
-    public function getFlight($from, $fromDate, $price)
+    public function getFlight($from, $to, $fromDate, $price)
     {
 
         $flights = Flight::where('departure_id', $from->id)
+            ->where('arrival_id', $to->id)
             ->where('leaves', '>=', $fromDate)
             ->where('price', '<=', $price)
             ->with('departure')
@@ -121,8 +121,7 @@ class FlightController extends Controller
             $flights->arrival->preferences;
             return $flights;
             }
-            $to = City::where('name', '!=', $from)->get()->first();
-            return $this->store($from, $to, $fromDate);
+            return $this->store($from, $to, $fromDate, $price);
     }
 
     public function getLastFlight($from, $to, $fromDate, $price)
@@ -134,7 +133,6 @@ class FlightController extends Controller
             ->with('departure')
             ->with('arrival')
             ->with('company')
-            //->orderBy('distance', 'ASC')
             ->orderBy('leaves', 'ASC')
             ->get()->first();
         
@@ -143,23 +141,66 @@ class FlightController extends Controller
             $flights->arrival->preferences;
             return $flights;
         }
-        return $this->store($from, $to, $fromDate);
+        return $this->store($from, $to, $fromDate, $price);
     }
 
 
-    public function getRoundtrips($from, $fromDate, $toDate, $noDst, $price, $prefferences){
+    public function decodePreferences($preferences){
+        $prefs = [];
+
+        if($preferences[0]){
+            array_push($prefs, "Culture");
+        }
+        if($preferences[1]){
+            array_push($prefs, "Relaxation");
+        }
+        if($preferences[2]){
+            array_push($prefs, "Luxury flights");
+        }
+        if($preferences[3]){
+            array_push($prefs, "Beach resort");
+        }
+        if($preferences[4]){
+            array_push($prefs, "History");
+        }
+
+        if($preferences[5]){
+            array_push($prefs, "Social");
+        }
+
+        if($preferences[6]){
+            array_push($prefs, "Adventure");
+        }
+
+        if($preferences[7]){
+            array_push($prefs, "Premium services");
+        }
+
+        return $prefs;
+    }
+
+
+    public function getRoundtrips($from, $fromDate, $toDate, $noDst, $price, $culture, $relaxation, $luxuryFlights, $beachResort, $history, $social, $adventure, $premiumServices){
         $result = [];
 
+        $prefs = [$culture, $relaxation, $luxuryFlights, $beachResort, $history, $social, $adventure, $premiumServices];
+        
+        $preferences = $this->decodePreferences($prefs);
+
+        $preferences_checked = 0;
+
+        $from = City::where('name', $from)->first();
+
         for ($i = 0; $i <= rand(4, 6); $i++) {
-            array_push($result, $this->getRoundtrip($from, $fromDate, $toDate, $noDst, $price, $prefferences));
+            array_push($result, $this->getRoundtrip($from, $fromDate, $toDate, $noDst, $price, $preferences, $preferences_checked));
         }
 
         return json_encode($result);
     }
 
-    public function getRoundtrip($from, $fromDate, $toDate, $noDst, $price, $prefferences)
+    public function getRoundtrip($from, $fromDate, $toDate, $noDst, $price, $preferences, $preferences_checked)
     {
-        $city = City::where('name', $from)->with('preferences')->get()->first();
+        $home = $from;
 
         $theDate = new DateTime($fromDate);
         
@@ -175,35 +216,47 @@ class FlightController extends Controller
         $interval = $interval*60;
 
         $midTime = new DateTime($fromDate);
-        //$midTime->add(new DateInterval('PT' . $interval . 'M'));
-        //$midTime->format('Y-m-d H:i:s');
-        $midPrice = $price / $noDst;
+        $maxPrice = $price / $noDst;
         $totalPrice = 0;
         $totalDistance = 0;
         $roundtrips = array();
-        $midCity = City::where('name', $from)->with('preferences')->get()->first();
-        for ($i = 1; $i <= $noDst; $i++) {
-            $flight = $this->getFlight($midCity, $midTime->format('Y-m-d H:i:s'), $midPrice);
+ 
+        $cities = City::where('name', '!=', $from->name)->inRandomOrder()->get(); //vsetky mesta okrem odkial idem
+        $cities_to_visit = [];
+        $count = $noDst; //3 zastavky ->  BA-LO[0]-NY[1]-TO[2]->BA 
+
+        foreach ($cities as $city) {
+            if ($count == 0) {
+                break;
+            }
+            $intersect = count(array_intersect($preferences, $city->preferences()->pluck('name')->toArray()));
+            
+            if ($intersect >= $preferences_checked){
+                array_push($cities_to_visit, $city);
+            }
+
+            $count -= 1;
+        }
+        $preferences_checked -= 1;
+        
+        
+        foreach ($cities_to_visit as $to) {
+            $flight = $this->getFlight($from, $to, $midTime->format('Y-m-d H:i:s'), $maxPrice);
             $flight->arrival->preferences;
             $flight->departure->preferences;
             $flight->company;
             $roundtrips[] = $flight;
             $totalPrice += $flight->price;
             $totalDistance +=  $flight->distance;
-            $midCity = City::where('id', $flight->arrival_id)->with('preferences')->get()->first();    
             $midTime = $midTime->add(new DateInterval('PT' . $interval . 'M')); 
-            //$roundtrips[] = $midTime->format('Y-m-d H:i:s');
+            $from = $to;
         }
 
-            $flight = $this->getLastFlight($midCity, $city, $midTime->format('Y-m-d H:i:s'), $midPrice);
+            $flight = $this->getLastFlight($from, $home, $midTime->format('Y-m-d H:i:s'), $maxPrice);
             $flight->company;
             $totalPrice += $flight->price;
             $totalDistance +=  $flight->distance;
             $roundtrips[] = $flight;
-        
-            //$roundtrips[] = $midCity->name;
-            //$roundtrips[] = $city->name;
-            //$roundtrips[] = $midTime;
 
             $result = array("flights"=>$roundtrips, "price"=>$totalPrice, "distance"=>$totalDistance);
             
